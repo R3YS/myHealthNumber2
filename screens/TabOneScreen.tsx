ed6312460d30ed5ff4cf9544092ed3c0ea1b8569
R3YS <89490059+R3YS@users.noreply.github.com>
2021-11-28 22:13:30 -0800

    Global Variables

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -15,6 +15,7 @@ import MaterialCommunityIconsIcon from "react-native-vector-icons/MaterialCommun
 import MaterialIconsIcon from "react-native-vector-icons/MaterialIcons";
 import EntypoIcon from "react-native-vector-icons/Entypo";
 import IoniconsIcon from "react-native-vector-icons/Ionicons";
+import './global.js';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
